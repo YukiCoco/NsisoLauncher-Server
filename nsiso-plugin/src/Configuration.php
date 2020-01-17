0ede6c8319e0f20d9ca67eddb61d017349202854
YukiCoco <theblackrains@outlook.com>
2020-01-17 16:28:10 +0800

    修改插件视图

--- nsiso-plugin/src/Configuration.php
+++ nsiso-plugin/src/Configuration.php
@@ -10,6 +10,11 @@ class Configuration
 {
     public function render()
     {
+        $md5Form = Option::form('common', '防作弊配置', function($form) {
+            $form->checkbox('ygg_enable_nsiso_check', 'nsiso 防作弊')
+                ->label('开启 nsiso 防作弊');
+            $form->textarea('md5_list','模组 md5 列表')->rows(5)->hint('使用工具获取模组的 md5 ,使用英文逗号隔开。');
+        })->handle();
         $path = plugin('nsiso')->getPath().'/README.md';
         $markdown = @file_get_contents($path);
         if (!$markdown) {
@@ -19,6 +24,6 @@ class Configuration
         }
         $ads = Ad::all();
         $adCount = $ads->count();
-        return view('Nsiso::config', compact('readme','ads','adCount'));
+        return view('Nsiso::config', compact('readme','ads','adCount','md5Form'));
     }
 }
